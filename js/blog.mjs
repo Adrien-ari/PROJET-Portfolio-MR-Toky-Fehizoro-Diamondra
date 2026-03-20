14988343cf33ab0a0ad4bf6b3d4e3bfe122c7c2a
Fehizoro <fehizoro.2@mail.hei.school>
2026-03-20 15:04:14 +0300

    Modif : modification du blog : ajout de Aside, newsletter

--- js/blog.mjs
+++ js/blog.mjs
@@ -13,9 +13,7 @@ for (let i = 0; i < data.posts.length; i++) {
 
   document
     .querySelector(`.postContent${i}`)
-    .appendChild(
-      createElement("div", `blogTitleDateContainer${i}`,''),
-    );
+    .appendChild(createElement("div", `blogTitleDateContainer${i}`, ""));
   document
     .querySelector(`.blogTitleDateContainer${i}`)
     .appendChild(createElement("p", `blogPostTitle${i}`, data.posts[i].title));
@@ -38,7 +36,6 @@ for (let i = 0; i < data.posts.length; i++) {
       .querySelector(`.blogPostTagsContainer${i}`)
       .appendChild(createElement("p", `tag${index}`, element));
   });
-
 }
 
 for (let index = 0; index < data.posts.length; index++) {
@@ -48,8 +45,56 @@ for (let index = 0; index < data.posts.length; index++) {
   document
     .querySelector(`.blogPostTagsContainer${index}`)
     .classList.add("commmonTagsClass");
-  document.querySelector(`.blogTitleDateContainer${index}`)
-  .classList.add('CommonTitleDateContainer');
   document
-    .querySelector(`.postContent${index}`).classList.add('commonPostContent');
+    .querySelector(`.blogTitleDateContainer${index}`)
+    .classList.add("CommonTitleDateContainer");
+  document
+    .querySelector(`.postContent${index}`)
+    .classList.add("commonPostContent");
 }
+
+document
+  .querySelector(".blogAdditifContent")
+  .appendChild(createElement("div", "archiveContainerBlog", ""));
+
+for (let i = 0; i < data.archives.length; i++) {
+  document
+    .querySelector(".archiveContainerBlog")
+    .appendChild(createElement("div", `archiveElement${i}`, ""));
+  document
+    .querySelector(`.archiveElement${i}`)
+    .appendChild(
+      createElement("p", `archiveElement${i}`, data.archives[i].label),
+    );
+  document
+    .querySelector(`.archiveElement${i}`)
+    .appendChild(
+      createElement("p", `archiveElementCount${i}`, data.archives[i].count),
+    );
+}
+
+document
+  .querySelector(".blogAdditifContent")
+  .appendChild(createElement("div", `newsletter`, ""));
+document
+  .querySelector(".newsletter")
+  .appendChild(createElement("p", `newsletter`, "NEWSLETTER"));
+document
+  .querySelector(".newsletter")
+  .appendChild(
+    createElement(
+      "p",
+      `newsletterIncitation`,
+      "Get new article straight to your inbox. No spam, ever.",
+    ),
+  );
+document
+  .querySelector(".newsletter")
+  .appendChild(
+    createElement(
+      "input",
+      `newsletterIncitationInput`,
+      ""
+    )
+  );
+
